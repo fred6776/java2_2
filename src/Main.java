867dd05543e24ca0b5211208649af0824aa10c27
Farid Mukhametkhanov <fred6776@yandex.com>
2025-06-13 18:06:14 +0300

    Create

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+        int startBalance = 100;    // Начальный счёт
+        int summaPop = 1100;    // Сумма пополнения
+        int bonusUp = 1000;   // Сумма от которой капает бонус
+        int bonus = 0;              // Сумма бонуса
+        int itogBalance = 0;       // Итоговый баланс
+
+        // превысила ли
+        // сумма пополнения порог
+        if (summaPop > bonusUp) {
+            bonus = summaPop / 100;
+        }
+
+        // Рассчитываем итоговый счёт
+        itogBalance = startBalance + summaPop + bonus;
+
+        //Вывод результата
+        System.out.println("Бонус: " + bonus + " рублей");
+        System.out.println("Итоговый счёт: " + itogBalance + " рублей");
+    }
+}
